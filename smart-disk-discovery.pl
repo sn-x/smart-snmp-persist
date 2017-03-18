ef520c6b1b2654b9305a3529cd541583e040af3c
aljaz <snixorcek@gmail.com>
2017-03-18 18:30:52 +0100

    bugfix, added an empty space to nvme command

--- smart-disk-discovery.pl
+++ smart-disk-discovery.pl
@@ -231,7 +231,10 @@ sub nvmeSMARTD {
 	my $controller   = `ls \"/sys/bus/pci/devices/$handle/misc/\"`; # get controller name from disk location
 	chomp $controller ; #remove newline from end of string
 
-	push (@self, ($SMARTCTL . "-a /dev/" . $controller . " -d " . $input->{driver})); # add smart command to array
+	`$SMARTCTL . " -a /dev/" . $controller . " -d " . $input->{driver}` # probe for drive
+	if (($? != 256) && ($? != 512)) { # if smartd succeeded
+		push (@self, ($SMARTCTL . " -a /dev/" . $controller . " -d " . $input->{driver})); # add smart command to array
+	}
 
 	return @self; # return array of smartctl commands
 }
