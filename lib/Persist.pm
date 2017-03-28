16c9a7bb3fcbe287b82c006b512f7c465d1bf8dd
aljaz <snixorcek@gmail.com>
2017-03-28 16:19:19 +0200

    fixed bug in Persist.pm

--- lib/Persist.pm
+++ lib/Persist.pm
@@ -2,17 +2,17 @@
 
 package Persist;
 
-#use strict;
-#use warnings;
+use strict;
+use warnings;
 use File::Slurp;
 use Data::Dumper;
 use XML::Simple;
 use SNMP::Extension::PassPersist;
 
 my $base_oid    = ".1.3.6.1.3.3";
-my $full_oid    = $base_oid . ".1";
-my $small_oid   = $base_oid . ".2";
-my $nvme_oid    = $base_oid . ".3";
+my $full_oid    = $base_oid . ".0";
+my $small_oid   = $base_oid . ".1";
+my $nvme_oid    = $base_oid . ".2";
 
 ############################################################
 
@@ -39,6 +39,7 @@ sub tree {
 	my $oid;
 			
 	foreach my $drive (keys %hash) {
+		my $oid;
 		$oid = unique_oid($hash{$drive}, $unique);
 
 		push(@array, oid_tree($oid, $hash{$drive})) if $hash{$drive}{'big_table'};
@@ -47,9 +48,6 @@ sub tree {
 	    
 		$unique++;
 	}
-
-#print Dumper(@array);
-#exit;
 
 	return @array;
 }
@@ -69,19 +67,20 @@ sub oid_tree {
 	my %device      = %{$self};
 	my @array;
 
-	push(@array, ($oid . ".1" => ['string',  $device{'vendor'}]));		
-    push(@array, ($oid . ".2" => ['string',  $device{'model'}]));
-	push(@array, ($oid . ".3" => ['string',  $device{'serial'}]));
+	push(@array, ($oid . ".1"   => ['string',  $device{'vendor'}]));		
+	push(@array, ($oid . ".2"   => ['string',  $device{'model'}]));
+	push(@array, ($oid . ".3"   => ['string',  $device{'serial'}]));
 	push(@array, ($oid . ".100" => ['integer', $device{'exitcode'}]));
 
 	for my $smart_id (keys %{$device{'big_table'}}) {
 		my $original_id = $smart_id;
 		$smart_id =~ s/smart_/./g;
-		$full_oid = $oid . ".101" . $smart_id;
 
-		push(@array, ($full_oid => ['string',  $device{'big_table'}{$original_id}]));		
+		my $full_oid = $oid . ".101" . $smart_id;
+
+		push(@array, ($full_oid => ['integer',  $device{'big_table'}{$original_id}]));		
 	}
-	
+		
 	return @array;
 }
 
