60dcfd614553462087f504725ecb5102ca3bfdb8
Aljaz Meznaric <snixorcek@gmail.com>
2017-04-05 16:31:35 +0200

    fixed dereferncing problems with older perl

--- nagios-plugin/nagios-snmp-smart.pl
+++ nagios-plugin/nagios-snmp-smart.pl
@@ -5,7 +5,7 @@ use warnings;
 use Net::SNMP;
 use Data::Dumper;
 
-my $oid='.1.3.6.1.3.3';
+my $oid='.1';
 
 nagios_output();
 
@@ -20,13 +20,13 @@ sub nagios_output {
 }
 
 sub find_drives {
-	my $snmp_data = fetch_snmp_table();
+	my $snmp_data_hash_ref = fetch_snmp_table();
+	my %snmp_data_hash     = %{$snmp_data_hash_ref};
 	my $drive     = 1;
 	my $found     = 0;
 	my $results;
 
-	while (%{$snmp_data}{$oid.".".$drive.".1.2"}) {
-		#print Dumper(%{$snmp_data}{$oid.".".$drive.".1.2"});
+	while ($snmp_data_hash{$oid.".".$drive.".1.2"}) {
 		$found++;
 		$drive++;
 	}
